25f8615a9c01b63e14842d39c468373bc7396e21
Jon Lunsford <jon@capturethecastle.net>
2022-03-16 13:26:33 -0700

    set honebadger envs

--- config/dev.exs
+++ config/dev.exs
@@ -67,6 +67,9 @@ config :story, StoryWeb.Endpoint,
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
 
+config :honeybadger,
+  environment_name: :dev
+
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
 config :phoenix, :stacktrace_depth, 20
